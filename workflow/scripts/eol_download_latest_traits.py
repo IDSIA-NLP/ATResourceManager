82b8afb5921c314d791b90301e88be04bca09ca2
hdetering <harald.detering@gmail.com>
2022-07-31 16:52:25 +0200

    Implemented EOL download script.

--- workflow/scripts/eol_download_latest_traits.py
+++ workflow/scripts/eol_download_latest_traits.py
@@ -8,15 +8,13 @@
 #------------------------------------------------------------------------------
 # author   : Harald Detering
 # email    : harald.detering@gmail.com
-# modified : 2022-07-29
+# modified : 2022-07-31
 #------------------------------------------------------------------------------
 
+import click
 import os, sys
-import argparse
 import requests
 from dateutil.parser import parse
-#from bs4 import BeautifulSoup
-#import pandas as pd
 import re
 
 # ------------------------------------------------------------------------------
@@ -26,46 +24,49 @@ import re
 url = 'https://editors.eol.org/other_files/SDR/traits_all.zip'
 
 # ------------------------------------------------------------------------------
+#                               UTILITY FUNCTIONS
+# ------------------------------------------------------------------------------
+
+def download_file(url_dl, out_dir):
+  if not os.path.exists(out_dir):
+    print(f'[ERROR] Directory does not exist: {out_dir}.')
+    sys.exit(1)
+
+  # get filename from URL
+  filename = url_dl.strip().rsplit('/', 1)[1]
+  # build file path to write to
+  fn_out = os.path.join(out_dir, filename)
+  
+  print(f'source: {url_dl}')
+  print(f'target: {fn_out}')
+  print()
+
+  response = requests.get(url_dl, stream=True)
+  with open(fn_out, 'wb') as f:
+    i = 0
+    for chunk in response.iter_content(chunk_size=512*1024):
+      f.write(chunk)
+      i += 1
+      print(f'{512 * i} kb downloaded...', end='\r')
+    LINE_CLEAR = '\x1b[2K' # <-- ANSI sequence
+    print(end=LINE_CLEAR)
+  
+  return fn_out
+  
+# ------------------------------------------------------------------------------
 #                            COMMAND-LINE INTERFACE
 # ------------------------------------------------------------------------------
 
-def parse_args():
-  parser = argparse.ArgumentParser(description='Download Encyclopedia of Life (EOL) resources.')
-  parser.add_argument('--version-file', help='Current version of downloaded EOL data.')
-  parser.add_argument('out_dir', help='Output directory to download to.')
-
-  args = parser.parse_args()
-  return args
-
-def download_file(url, out_dir):
-    if not os.path.exists(args.out_dir):
-      print(f'[ERROR] Directory does not exist: {args.out_dir}.')
-      sys.exit(1)
-    
-    # build URL to download from
-    url_dl = f'{base_url}/{filename}'
-    # build file path to write to
-    fn_out = os.path.join(args.out_dir, filename)
-    
-    print(f'source: {url_dl}')
-    print(f'target: {fn_out}')
-    print()
-
-    response = requests.get(url_dl, stream=True)
-    with open(fn_out, 'wb') as f:
-      i = 0
-      for chunk in response.iter_content(chunk_size=512*1024):
-        f.write(chunk)
-        i += 1
-        print(f'{512 * i} kb downloaded...', end='\r')
-      LINE_CLEAR = '\x1b[2K' # <-- ANSI sequence
-      print(end=LINE_CLEAR)
-
-def main(args):
-  version = ''
+@click.command()
+@click.option('--version-file', '-v', help='File containing version of downloaded EOL resources.')
+@click.option('--out-dir', '-o', required=True, help='Output directory to download to.')
+def main(version_file, out_dir): #def main(args):
+  """Download Encyclopedia of Life (EOL) resources."""
+  version = '1970-01-01 00:00:00 GMT'
+  dt_version = parse(version)
   # read version file if given
-  if args.version_file:
-    with open(args.version_file, 'rt') as f:
+  if version_file:
+    with open(version_file, 'rt') as f:
       version = f.readline().strip()
       dt_version = parse(version)
   print(f'Latest version downloaded:\n{dt_version}\n---')
@@ -82,12 +83,12 @@ def main(args):
     print(f'  Local version:  {dt_version}')
     print()
     print('---\nNow downloading data...')
-    #out_fn = download_file(url, args.out_dir)
-    #print(f'Download successful. Find new data at:\n{os.path.join(args.out_dir, web_version)}')
-    if args.version_file:
-      print(f'Storing latest version in version file {args.version_file}.')
-  #    with open(args.version_file, 'wt') as f:
-  #      f.write(f'{web_version}\n')
+    fn_out = download_file(url, out_dir)
+    print(f'Download successful. Find new data at:\n{fn_out}')
+    if version_file:
+      print(f'Storing latest version in version file {version_file}.')
+      with open(version_file, 'wt') as f:
+        f.write(f'{dt_mod_date}\n')
   else:
     print(f'Already on latest version:\n{dt_mod_date}')
     print()
@@ -95,5 +96,7 @@ def main(args):
   
 
 if __name__ == '__main__':
-  args = parse_args()
-  main(args)
+  #args = parse_args()
+  #main(args)
+
+  main()
